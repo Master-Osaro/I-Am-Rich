f69928fa084ed8894d73bc1971e52d75eece78b2
Osaro Iyoha <saro.iyoha@gmail.com>
2020-03-29 22:56:40 +0100

    Initial Upload

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Osaro Iyoha on 29/03/2020.
+//  Copyright © 2020 Osaro Iyoha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
